ee2987690f362741bf8d6b624e0bf1946a7484ea
Md. Abu Huraira Rahi <mabuhurairarahi@gmail.com>
2026-02-24 18:50:41 +0000

    add functions files

---
+++ Function/call by value.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+//call by value
+void callbyValue (int Variable)
+{
+    Variable += 10;
+    cout << "Variable --> value inside the function: " << Variable << endl;
+}
+
+int main()
+{
+    int Variable = 10;
+    callbyValue(Variable);
+
+    /*
+
+    #Call by Value: when you pass a variable to a function,the function receives a copy
+    of that variable — not the original. So, any changes made inside the function do NOT affect
+    the original variable.
+    
+    */
+
+    cout << "Variable --> value in the main() function: " << Variable << endl;
+
+    return 0;
+}
