6a3396f48baa19e43f65cb5db6fb6b9d9bc755e8
Md. Abu Huraira Rahi <mabuhurairarahi@gmail.com>
2026-02-26 00:23:22 +0000

    add header files

--- Header File/Type-02/headerOne.h
+++ Header File/Type-02/headerOne.h
@@ -1,6 +1,23 @@
 #ifndef HEADERONE_H
 #define HEADERONE_H
 
+     /*
+     
+     #ifndef HEADERFILENAME_H   ---> it's called pre-processor directive
+     #define HEADERFILENAME_H   ---> it's macro
+
+     #enfif
+
+     this upper structure is known as header-guard. 
+     
+     "#ifndef" means if-not-define, e.g. #ifndef HEADERFILENAME_H --> by declaring this compiler checks is any macro
+     named "HEADERFILENAME_H" available or not.
+
+     "#enfif" --> header-guard ends here.
+
+     
+     */
+
 int sum(int varOne, int varTwo);
 
 #endif
