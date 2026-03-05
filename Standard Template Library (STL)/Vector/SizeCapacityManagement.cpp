4cc1e29c8990e89f7a63504b6cb51d5aa56adbe8
Md. Abu Huraira Rahi <mabuhurairarahi@gmail.com>
2026-03-05 15:27:08 +0000

    add vector files

--- Standard Template Library (STL)/Vector/SizeCapacityManagement.cpp
+++ Standard Template Library (STL)/Vector/SizeCapacityManagement.cpp
@@ -35,5 +35,39 @@ int main()
     cout << "size: " << vectorOne.size() << endl;   //size: 4
     cout << "capacity: " << vectorOne.capacity() << endl;   //capacity: 5
 
+
+    //resizing a vector
+    vectorOne.resize(5);
+
+    /* --> it resizes the vector with new size by storing the previous existed data
+    and rest of the empty index will fill up with "NULL" value. */
+
+    cout << "size: " << vectorOne.size() << endl;
+    cout << "capacity: " << vectorOne.capacity() << endl;
+
+    /*
+    
+    #Attention for CAPACITY:
+
+    while resizing, if the "new_size" is less than "previous_capacity" the capacity will remain
+    unchanged. 
+    
+    E.g. : previous_size: 9 && previous_capacity: 10, 
+    resize(5) --> new_size: 5 && new_capacity: 10
+
+    #Remember: size will always change but the capacity will only change when the new_size
+    is greater than the previous_capacity.
+    
+    */
+
+
+    vector<char> charVector;
+
+    //pre-allocating space for charVector as form of "capacity"
+    charVector.reserve(10);
+
+    cout << "capacity: " << charVector.capacity() << endl;   //capacity: 10
+    cout << "size: " << charVector.size();  //size: 0
+
     return 0;
 }
